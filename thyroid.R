fc9dda9b841efa5daa03b06ead88895d31687f2b
Seongmin Choi <soymintc@gmail.com>
2021-07-16 07:13:00 +0000

    apply SCTransform

--- thyroid.R
+++ thyroid.R
@@ -3,25 +3,24 @@ library(ggplot2)
 library(patchwork)
 library(dplyr)
 
+# Get data in multiple directories
 setwd('~/local/seurat_raw_data')
 dl <- list.dirs(".", full.names = F, recursive=FALSE)
 names(dl) <- c("ATC1","ATC3","PTC1","PTC2","ATC2","PTC3","PTC4","ATC4") # set names to vector
 marrow.counts <- Read10X(dl) # names:item vector -> create reads data
-
 thyroid <- CreateSeuratObject(counts = marrow.counts, project = "Thyroid")
-thyroid[["percent.mt"]] <- PercentageFeatureSet(thyroid, pattern = "^MT-")
+
+thyroid[["percent.mt"]] <- PercentageFeatureSet(thyroid, pattern = "^MT-") # calc read% from mitochondrial genes
 
 table(thyroid@meta.data$orig.ident) # check overall data
 
-thyroid@meta.data$orig.ident %>%
-  head()
-
 VlnPlot(thyroid, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 
+# In-filter for cells
 thyroid <- subset(thyroid, subset = 
                  nFeature_RNA > 200 & # rm low or high freq expr
                  nFeature_RNA < 2500 & 
-                 percent.mt < 10) # rm high mt expr ratio
+                 percent.mt < 25) # rm high mt expr ratio
 
 # Visualize highly expressed genes
 C = thyroid@assays$RNA@counts # counts dataset only
@@ -40,30 +39,6 @@ rownames(thyroid)[grepl("^RPS", rownames(thyroid))]
 thyroid = thyroid[!grepl("^RPL", rownames(thyroid)),]
 thyroid = thyroid[!grepl("^RPS", rownames(thyroid)),]
 
-# # Normalize data
-# thyroid <- NormalizeData(thyroid, 
-#                          normalization.method = "LogNormalize",
-#                          scale.factor = 10000)
-# # Identify highly variable genes
-# thyroid <- FindVariableFeatures(thyroid, selection.method = "vst", nfeatures = 2000)
-# 
-# # top 10 genes plot
-# top10 <- head(VariableFeatures(thyroid), 10) # top 10 of the HVG
-# plot1 <- VariableFeaturePlot(thyroid, log = TRUE)
-# plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-# plot2
-# 
-# # Scale data: mu=0, sd=1
-# all.genes <- rownames(thyroid)
-# thyroid <- ScaleData(thyroid, features = all.genes)
-
-# # Linear transformation
-# thyroid <- RunPCA(thyroid, features = VariableFeatures(object = thyroid)) # only for HVGs
-# VizDimLoadings(thyroid, dims = 1:2, reduction = "pca") # test PCA
-# DimPlot(thyroid, reduction = "pca")
-# DimHeatmap(thyroid, dims = 1, cells = 500, balanced = TRUE)
-# DimHeatmap(thyroid, dims = 1:30, cells = 500, balanced = TRUE)
-
 # SCTransform: replaces "NormalizeData, FindVariableFeatures, ScaleData
 thyroid <- SCTransform(thyroid, assay = "RNA", verbose = TRUE, method = "poisson")
 
@@ -75,18 +50,3 @@ thyroid <- RunUMAP(thyroid, reduction = "pca", dims = 1:30)
 
 # Plot UMAP by cluster-identity and given-identity
 DimPlot(thyroid, reduction = "umap", group.by = c("ident", "orig.ident"))
-
-# # NOTE: This process can take a long time for big datasets, comment out for expediency. More
-# # approximate techniques such as those implemented in ElbowPlot() can be used to reduce
-# # computation time
-# thyroid <- JackStraw(thyroid, num.replicate = 100)
-# thyroid <- ScoreJackStraw(thyroid, dims = 1:30)
-# JackStrawPlot(thyroid, dims = 1:30)
-# ElbowPlot(thyroid) # approximating the "true dimensionality / dim rank" of dataset
-
-# # Clustering
-# thyroid <- FindNeighbors(thyroid, dims = 1:30) # Jaccard similarity: |A,B|/|AvB|
-# thyroid <- FindClusters(thyroid, verbose=TRUE) # iteratively grouping genes hierarchially
-# thyroid <- RunUMAP(thyroid, reduction = "pca", dims = 1:30)
-# DimPlot(thyroid, reduction = "umap", group.by = c("ident", "orig.ident"))
-
